6fea94e1eff3958928dd81efa249e80fc84ecba9
Thomas Robillard <thomas.robillard@trojans.dsu.edu>
2025-05-01 14:40:17 -0500

    Created mousedown event for all textblocks

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -52,8 +52,28 @@ namespace MatchGame
             }
         }
 
-        
-       
+        TextBlock lastTextBoxClicked;
+        bool findingMatch = false;
 
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBoxClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBoxClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBoxClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
     }
 }
